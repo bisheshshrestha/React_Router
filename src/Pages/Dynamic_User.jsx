eb5105b7698c8b0305d1f2661aab53330745d278
bisehshshrestha <bisheshshrestha652@gmail.com>
2026-02-09 12:53:38 +0545

    feat: useParam dynamic routing

--- src/Pages/Dynamic_User.jsx
+++ src/Pages/Dynamic_User.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Dynamic_User = () => {
-  return (
-    <div>Dynamic_User</div>
-  )
-}
+  // console.log("UserParams = ", useParams());
+  const { id } = useParams();
+  const user = [
+    { id: 1, name: "Bishesh", gmail: "bishesh@gmail.com", age: 24 },
+    { id: 2, name: "Sushmita", gmail: "sushmita@gmail.com", age: 23 },
+    { id: 3, name: "Superman", gmail: "superman@gmail.com", age: 20 },
+    { id: 4, name: "Batman", gmail: "batman@gmail.com", age: 20 },
+  ];
+  const specific_user = user.find((data) => data.id == id);
+  console.log(specific_user);
 
-export default Dynamic_User
+  return (
+    <div>
+      <h1>Id = {specific_user.id}</h1>
+      <h1>Name= {specific_user.name}</h1>
+      <h1>Gmail= {specific_user.gmail}</h1>
+      <h1>Age= {specific_user.age}</h1>
+    </div>
+  );
+};
+
+export default Dynamic_User;
